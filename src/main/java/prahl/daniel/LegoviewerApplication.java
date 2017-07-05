7accf2f496259fc50156cbbf48e33e744fadd2dd
Daniel Prahl <daniel.prahl@gmail.com>
2017-07-05 13:49:21 -0400

    "initial commit"

---
+++ src/main/java/prahl/daniel/LegoviewerApplication.java
@@ -0,0 +1,12 @@
+package prahl.daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoviewerApplication.class, args);
+	}
+}
