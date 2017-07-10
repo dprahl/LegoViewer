f027aec5c0309d088561fa77236a6c5f87cfb44a
Daniel Prahl <daniel.prahl@gmail.com>
2017-07-10 12:07:47 -0400

    "cleaned up some ionic code for passing data between pages"

--- src/main/java/prahl/daniel/Util.java
+++ src/main/java/prahl/daniel/Util.java
@@ -4,12 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by danielprahl on 7/5/17.
